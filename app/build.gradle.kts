023efb691c4496187785fda3a6ff7cbaa29f2e3f
Micaela <micaelahimura@hotmail.com>
2025-04-15 20:40:59 -0300

    chore: chore: migrate test framework from JUnit 4 to JUnit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment:2.6.0")
     implementation("androidx.navigation:navigation-ui:2.6.0")
-    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
