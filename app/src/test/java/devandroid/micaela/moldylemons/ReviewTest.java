d1369b7e543a75dcd85e74fb3d773444cb308582
Micaela <micaelahimura@hotmail.com>
2025-05-08 22:21:50 -0300

    test: Adjust unit tests to support the new mediaId field - Add new test cases to validate the mediaId field - add test cases for coupleId field
    refs #12, #9

--- app/src/test/java/devandroid/micaela/moldylemons/ReviewTest.java
+++ app/src/test/java/devandroid/micaela/moldylemons/ReviewTest.java
@@ -20,7 +20,7 @@ public class ReviewTest{
         this.title = "Best movie ever!";
         this.content = "This movie is very funny";
         this.writtenBy = "Mariah";
-        this.review = new Review(1,title ,content , writtenBy, "😊",5, 1);
+        this.review = new Review(1,title ,content , writtenBy, "😊",5, 1,1);
     }
 
     @Test
@@ -32,11 +32,12 @@ public class ReviewTest{
         assertEquals( this.writtenBy,  this.review.getWrittenBy());
         assertEquals(5,  this.review.getRating());
         assertEquals("😊",  this.review.getReactionEmoji());
+        assertEquals(1, this.review.getMediaId());
     }
 
     @Test
     void givenOnlyRequiredFields_whenCreatingReview_thenReviewIsCreatedSuccessfully() {
-        Review review = new Review(1, null, null, "John", "👍", 4, 1);
+        Review review = new Review(1, null, null, "John", "👍", 4, 1,1);
 
         assertEquals(1, review.getId());
         assertNull(review.getTitle());
@@ -44,19 +45,20 @@ public class ReviewTest{
         assertEquals("John", review.getWrittenBy());
         assertEquals(4, review.getRating());
         assertEquals("👍", review.getReactionEmoji());
+        assertEquals(1, this.review.getMediaId());
     }
 
     @Test
     void givenNullRequiredFields_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            Review review = new Review(0, null, null, null, null, 0, 1);
+            Review review = new Review(0, null, null, null, null, 0, 1,0);
         });
     }
 
     @Test
     void givenEmptyWrittenBy_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", "", "😐", 3, 1);
+            new Review(2, "Title", "Description", "", "😐", 3, 1,1);
         });
 
         assertEquals("Author cannot be null or empty.", exception.getMessage());
@@ -65,7 +67,7 @@ public class ReviewTest{
     @Test
     void givenNullWrittenBy_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", null, "😐", 3, 1);
+            new Review(2, "Title", "Description", null, "😐", 3, 1,1);
         });
 
         assertEquals("Author cannot be null or empty.", exception.getMessage());
@@ -74,7 +76,7 @@ public class ReviewTest{
     @Test
     void givenNullReactionEmoji_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", "Carla", null, 5, 1);
+            new Review(2, "Title", "Description", "Carla", null, 5, 1,1);
         });
 
         assertEquals("Reaction must be a valid emoji.", exception.getMessage());
@@ -82,7 +84,7 @@ public class ReviewTest{
     @Test
     void givenInvalidReactionEmoji_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", "Carla", "abc123", 5,1);
+            new Review(2, "Title", "Description", "Carla", "abc123", 5,1,1);
         });
 
         assertEquals("Reaction must be a valid emoji.", exception.getMessage());
@@ -91,7 +93,7 @@ public class ReviewTest{
     @Test
     void givenZeroRating_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", "Meggie", "😐", 0, 1);
+            new Review(2, "Title", "Description", "Meggie", "😐", 0, 1,1);
         });
 
         assertEquals("Rating must be between 1 and 5.", exception.getMessage());
@@ -99,14 +101,23 @@ public class ReviewTest{
     @Test
     void givenInvalidRating_whenCreatingReview_thenThrowsIllegalArgumentException() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Review(2, "Title", "Description", "Meggie", "😐", 15, 1);
+            new Review(2, "Title", "Description", "Meggie", "😐", 15, 1,1);
         });
 
         assertEquals("Rating must be between 1 and 5.", exception.getMessage());
     }
+    @ParameterizedTest
+    @ValueSource(ints = { 0, -1 })
+    void givenInvalidMediaId_whenCreatingReview_thenThrowsIllegalArgumentException(int invalidMediaId) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Review(2, "Title", "Description", "Meggie", "😐", 5, 1,invalidMediaId);
+        });
+
+        assertEquals("Must be a valid ID.", exception.getMessage());
+    }
 
     @Test
-    void givenInvalidRating_whenUpdatingReview_thenThrowsIllegalArgumentException(){
+    void givenInvalidRating_whenSettingRating_thenThrowsIllegalArgumentException(){
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             review.setRating(-1);
         });
@@ -140,7 +151,7 @@ public class ReviewTest{
     @ParameterizedTest
     @ValueSource(strings = {"😀", "🌍", "🚀", "⚗️", "🟠", "🠔", "🤖", "🫠", "☀️", "✂️","🇧🇷"})
     void givenValidEmojis_whenSettingReactionEmoji_thenDoesNotThrow(String emoji) {
-        Review review = new Review(1, "A", "B", "C", "😊", 5, 1);
+        Review review = new Review(1, "A", "B", "C", "😊", 5, 1,1);
         assertDoesNotThrow(() -> this.review.setReactionEmoji(emoji));
     }
 
@@ -175,11 +186,37 @@ public class ReviewTest{
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             review.setId(invalidId);
         });
-        assertEquals("ID must be greater than zero.", exception.getMessage());
+        assertEquals("Must be a valid ID.", exception.getMessage());
     }
     @Test
     void givenValidId_whenSettingId_thenSucceeds(){
         this.review.setId(4);
         assertEquals(4, review.getId());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void givenInvalidCoupleId_whenSettingCoupleId_thenThrowsIllegalArgumentException(int invalidCoupleId){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            review.setCoupleId((invalidCoupleId));
+        });
+        assertEquals("Must be a valid ID.", exception.getMessage());
+    }
+    @Test
+    void givenValidCoupleId_whenSettingCoupleId_thenSucceeds(){
+        this.review.setCoupleId(4);
+        assertEquals(4, review.getCoupleId());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void givenInvalidMediaId_whenSettingMediaId_thenThrowsIllegalArgumentException(int invalidMediaId){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            review.setMediaId(invalidMediaId);
+        });
+        assertEquals("Must be a valid ID.", exception.getMessage());
+    }
+    @Test
+    void givenValidMediaId_whenSettingMediaId_thenSucceeds(){
+        this.review.setMediaId(4);
+        assertEquals(4, review.getMediaId());
     }
 }
